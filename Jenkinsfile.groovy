74778e634ae3a986c8c704e24653668739f8e14e
Wimukthi Indeewara <wind3907>
2022-08-24 15:39:42 +0530

    test

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,33 @@
+properties(
+    [
+        buildDiscarder(logRotator(numToKeepStr: '20')),
+        parameters(
+            [   
+                string(name: 'PREFIX', defaultValue: 'lx', description: 'Host name prefix'),
+            ]
+        )
+    ]
+)
+pipeline {
+    agent { label 'master' }
+    stages {
+        stage('Test') {
+            steps {
+                echo "Section: Verifying parameters"
+                sh """
+                    echo "hi"
+                """
+            }
+        }
+    }
+    stages {
+        stage('Hello') {
+            steps {
+                echo "Section: Verifying parameters"
+                sh """
+                    echo "hi"
+                """
+            }
+        }
+    }
+}
